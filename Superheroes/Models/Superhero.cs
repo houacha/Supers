89d0403976b6b8f604e2815455364ae9a0c54e67
houa cha <itshoua@gmail.com>
2020-01-02 10:39:49 -0600

    made superhero class and controller

---
+++ Superheroes/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superheroes.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AlterEgo { get; set; }
+        public string Ability { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+    }
+}
